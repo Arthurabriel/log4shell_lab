efa97a91d674fd0b62c91b0bedab62a3d10d5123
Arthurabriel <arthurgb62@gmail.com>
2026-03-19 09:40:42 -0300

    Coment Vulnerability

--- src/main/java/com/Main.java
+++ src/main/java/com/Main.java
@@ -5,6 +5,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Sistema de Login Iniciado...");
         String userInput = args.length > 0 ? args[0] : null;
-        VulnerableLogin.logFailedAuthentication(userInput);
+        //VulnerableLogin.logFailedAuthentication(userInput);
     }
 }
